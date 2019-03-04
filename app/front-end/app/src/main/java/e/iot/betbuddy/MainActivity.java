30530b16732b50b9b089074cccfe67956a6caa71
paulesta55 <paul.estano@ensea.fr>
2019-03-04 20:04:00 +0100

    add login activity

--- app/front-end/app/src/main/java/e/iot/betbuddy/MainActivity.java
+++ app/front-end/app/src/main/java/e/iot/betbuddy/MainActivity.java
@@ -3,11 +3,33 @@ package e.iot.betbuddy;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Arrays;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final int RC_SIGN_IN = 123;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        if (auth.getCurrentUser() != null) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);    // already signed in
+        } else {
+            startActivityForResult(
+                    // Get an instance of AuthUI based on the default app
+                    AuthUI.getInstance().createSignInIntentBuilder().setAvailableProviders(Arrays
+                            .asList(
+                            new AuthUI.IdpConfig.GoogleBuilder().build(),
+                            new AuthUI.IdpConfig.EmailBuilder().build(),
+
+                            new AuthUI.IdpConfig.AnonymousBuilder().build()))
+                            .build(),
+                    RC_SIGN_IN);
+        }
+
     }
 }
