70ed4d6882834dbc77c8b86fdaebfec77f5be2ff
Dhruv Samdani <dsamdani@gatech.edu>
2019-03-24 22:22:27 +0100

    group fragment starts activity correctly

--- app/front-end/app/src/main/java/e/iot/betbuddy/GroupFragment.java
+++ app/front-end/app/src/main/java/e/iot/betbuddy/GroupFragment.java
@@ -1,5 +1,6 @@
 package e.iot.betbuddy;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,11 +8,34 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import e.iot.betbuddy.adapters.GroupAdapter;
+import e.iot.betbuddy.data.DataHolder;
+import e.iot.betbuddy.model.Group;
+import e.iot.betbuddy.model.User;
 
 public class GroupFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_group, container, false);
+        View view = inflater.inflate(R.layout.fragment_group, container, false);
+
+        ListView groupListView = view.findViewById(R.id.group_ListView);
+        GroupAdapter groupAdapter  = new GroupAdapter(this.getActivity());
+        groupListView.setAdapter(groupAdapter);
+
+        groupListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                User user = (User)(DataHolder.getInstance().retrieve("user"));
+                Group group = user.getGroupList().get(position);
+                DataHolder.getInstance().save("group",group);
+                startActivity(new Intent(getActivity(), MessageActivity.class));
+            }
+        });
+
+        return view;
     }
 }
